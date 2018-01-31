db54f8ebae7a809671919a74fdc03df37aedfed4
Austin <austingheen@gmail.com>
2018-01-31 11:20:05 -0800

    add

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Post
+    {
+        public Post()
+        {
+            Created = DateTime.Now;
+            Value = 0;
+        }
+
+        public DateTime Created { get; set; }
+        public int Value { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public void Info()
+        {
+            Console.WriteLine("Created: " + Created);
+            Console.WriteLine("Title: " + Title);
+            Console.WriteLine("Descrption: " + Description);
+            Console.WriteLine("Value: " + Value);
+        }
+
+        public void UpVote()
+        {
+            Value += 1;
+        }
+
+        public void DownVote()
+        {
+            Value -= 1;
+        }
+
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post1 = new Post()
+            {
+                Title = "Hyperion Review",
+                Description = "Hyperion is a 9/10 over-all good read, I recommend"
+            };
+
+            var post2 = new Post()
+            {
+                Title = "Snow Crash Review",
+                Description = "SC is just an amazing book. Wowie! What a great read.",
+                Created = DateTime.Now.AddDays(5).AddHours(7)
+            };
+
+            post1.UpVote();
+            post1.UpVote();
+            post1.DownVote();
+            post1.Info();
+
+        }
+    }
+}
